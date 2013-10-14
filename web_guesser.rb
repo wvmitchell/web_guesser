20a7b7ed4923ebf39eb08308be5e99b59be6ed13
Will Mitchell <wvmitchell@gmail.com>
2013-10-14 14:19:06 -0600

    Generating rand number iteration 1

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+random ||= rand(100).to_s
 
 get '/hello' do
   "Hello, World"
+end
+
+get '/' do
+  "Hello, World. The secret number is #{random}"
 end
